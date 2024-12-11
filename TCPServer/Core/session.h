6cb193ade2df87d04fd94a0b15849a7d2e9e032f
Makga <79256138+srv-makga@users.noreply.github.com>
2024-12-11 20:34:15 +0900

    네트워트 작업

--- TCPServer/Core/session.h
+++ TCPServer/Core/session.h
@@ -1,12 +1,18 @@
 #pragma once
+#include "core_header.h"
 
 namespace core {
 namespace network {
 class Session
 {
 public:
+	using Id = uint64_t;
+
+public:
 	Session() = default;
 	virtual ~Session() = default;
+
+	virtual Id SessionId() const { return 0; }
 };
 } // namespace network
 } // namespace core
