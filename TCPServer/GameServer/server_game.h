d28ebc4325936d0e9b5602c4bc382be8d0210011
Makga <79256138+srv-makga@users.noreply.github.com>
2024-12-24 00:31:33 +0900

    게임 서버 변경

--- TCPServer/GameServer/server_game.h
+++ TCPServer/GameServer/server_game.h
@@ -1,46 +1,22 @@
 #pragma once
 
-#include "config_game.h"
-#include "../Common/server_base.h"
-#include "../Common/acceptor_base.h"
-#include "../Common/connector_base.h"
-#include "../Common/manager_base.hpp"
+#include "session_user.h"
+#include "acceptor_user.h"
 
-struct ConfigGame;
 
-class ServerGame : public ServerBase
+class GameServer
 {
 public:
-	using JobHandler_t = std::shared_ptr<JobHandler>;
-	using NetHandler_t = std::shared_ptr<NetHandler>;
+	GameServer(const core::network::IPEndPoint& _ep, SessionManager<std::shared_ptr<SessionUser>>* _session_manager);
+	GameServer() = delete;
+	GameServer(const GameServer& _other) = delete;
+	GameServer(GameServer&& _other) = delete;
+	GameServer& operator=(const GameServer& _other) = delete;
+	GameServer& operator=(GameServer&& _other) = delete;
 
-public:
-	ServerGame() = delete;
-	ServerGame(ConfigGame* _cconfig);
-	virtual ~ServerGame();
-
-	void Initialize();
+	bool Initialize();
 	void Finalize();
 
-	bool StartUp() override;
-	bool StartEnd() override;
-
-	bool InitConnector();
-	bool InitAcceptor();
-
-public:
-	ConnectorBase* Connector(fb::eServerType _server_type) const;
-	AcceptorBase* Acceptor(fb::eServerType _server_type) const;
-
 private:
-	JobHandler_t m_job_handler;
-	NetHandler_t m_net_handler;
-
-	std::vector<fb::eServerType> m_connect_list;
-	ManagerBase<fb::eServerType, ConnectorBase*> m_connectors;
-
-	std::vector<fb::eServerType> m_accept_list;
-	ManagerBase<fb::eServerType, AcceptorBase*> m_acceptors;
-
-	std::thread m_scheduler;
-};
+	AcceptorUser m_acceptor_user;
+};0
