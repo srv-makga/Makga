710168910734310069d61d25627b24715664f278
Makga <79256138+srv-makga@users.noreply.github.com>
2025-01-03 23:40:14 +0900

    유저 세션 슈퍼클래스 변경

--- TCPServer/GameServer/session_user.h
+++ TCPServer/GameServer/session_user.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "../Common/session_base.h"
+#include "../Core/iocp_session.h"
 #include "../Core/object_pool.h"
 #include "../Core/dispatcher.h"
 
@@ -8,7 +8,7 @@ class User;
 
 // 접속 종료가 되어도 재접속을 위해 유저 정보는 살아있어서 User가 Session을 직접 상속받지 않는다
 
-class SessionUser : public SessionBase
+class SessionUser : public core::network::IocpSession
 {
 public:
 	using Pid_t = fb::server::SendPid;
@@ -21,14 +21,21 @@ private:
 	static core::Dispatcher<Pid_t, Function_t> s_dispatcher;
 
 public:
-	SessionUser();
+	SessionUser(Id _id);
 	virtual ~SessionUser();
+	 
+	bool Initialize() override;
+	void Finalize() override;
 
-	void Initialize();
+	bool ProcPacket(NetPacket* packet);
+	ThreadId_t ThreadId() const;
 
-	bool RecvPacket(NetPacket* packet) override;
-	bool ProcPacket(NetPacket* packet) override;
-	ThreadId_t ThreadId() const override;
+protected:  // IocpSession
+	void OnConnected() override;
+	std::size_t OnRecv(char* buffer, std::size_t _length) override;
+	void OnRecvPacket(char* buffer, std::size_t _length) override;
+	void OnSend(std::size_t _length) override;
+	void OnDisconnected() override;
 
 protected:
 	bool OnLoginAuth(NetPacket* _packet);
