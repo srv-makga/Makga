e2c85473bca9c1bd7be288b30c8340d4dc3dd94a
Makga <79256138+srv-makga@users.noreply.github.com>
2025-03-10 23:06:36 +0900

    타입 변경

--- TCPServer/GameServer/game_type.h
+++ TCPServer/GameServer/game_type.h
@@ -6,6 +6,6 @@
 class Actor;
 class User;
 
-using ActorList = std::unordered_set<Actor*>;
+using ActorList = std::unordered_set<std::shared_ptr<Actor>>;
 using UserList = std::unordered_set<User*>;
 using ActorSearchFilter = uint32_t;
