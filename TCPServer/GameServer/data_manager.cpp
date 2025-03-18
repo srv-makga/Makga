f8c155ac06f77452eb5f50a339c7776f4feaab7f
Makga <79256138+srv-makga@users.noreply.github.com>
2025-03-18 22:24:18 +0900

    data manager 수정

--- TCPServer/GameServer/data_manager.cpp
+++ TCPServer/GameServer/data_manager.cpp
@@ -5,3 +5,19 @@ bool DataManager::Load()
 {
 	return true;
 }
+
+const ActorBasicTable* DataManager::FindActorBasicTable(MonsterIdx_t _idx) const
+{
+	return nullptr;
+}
+
+const MonsterBasicTable* DataManager::FindMonsterBasicTable(MonsterIdx_t _idx) const
+{
+	auto iter = m_monster_basic_table.find(_idx);
+	if (m_monster_basic_table.end() != iter)
+	{
+		return iter->second;
+	}
+
+	return nullptr;
+}
--- TCPServer/GameServer/data_manager.h
+++ TCPServer/GameServer/data_manager.h
@@ -1,15 +1,17 @@
 #pragma once
 
 #include "../Common/data_table.h"
-#include "../Core/singleton.hpp"
 
-class DataManager : public DataTable, public core::pattern::Singleton<DataManager>
+class DataManager : public DataTable
 {
 public:
 	DataManager() = default;
 
 	bool Load() override;
-};
 
-#define DATA_MANAGER	DataManager::Instance()
-#define SYSTEM			DataManager::Instance().SystemValue()
+	const ActorBasicTable* FindActorBasicTable(MonsterIdx_t _idx) const;
+	const MonsterBasicTable* FindMonsterBasicTable(MonsterIdx_t _idx) const;
+
+public:
+	std::map<MonsterIdx_t, MonsterBasicTable*> m_monster_basic_table;
+};
