142b961ba4b59b8b2a9ffc7f46309cf0c2f3ba03
Makga <79256138+srv-makga@users.noreply.github.com>
2025-04-25 23:24:28 +0900

    Update app_config.h

--- TCPServer/GameServer/app_config.h
+++ TCPServer/GameServer/app_config.h
@@ -1,16 +1,16 @@
 #pragma once
 
 #include "../Common/config.h"
-#include "../Core/singleton.hpp"
+#include "../Common/iocp_server.h"
+#include "../Common/iocp_client.h"
 
-struct AppConfig : public ServerConfig, public std::enable_shared_from_this<AppConfig>
+using Server_t = IocpServer;
+using Client_t = IocpClient;
+using Session_t = core::network::IocpSession;
+
+struct AppConfig : public ServerConfig<Session_t>, public std::enable_shared_from_this<AppConfig>
 {
-	int max_session_user_count = 3000;
 	int max_actor_monster = 10000;
 	int max_actor_npc = 1000;
 	int max_actor_object = 1000;
-
-	std::size_t buffer_size_user = 1 << 16;
 };
-
-#define SERVERLIST	CONFIG.serverlist
