1a127c95cd91d2e7113867fe78c506b8b6bb0f42
Makga <79256138+srv-makga@users.noreply.github.com>
2025-05-01 12:21:13 +0900

    updated appconfig

--- TCPServer/GlobalServer/app_config.h
+++ TCPServer/GlobalServer/app_config.h
@@ -5,7 +5,6 @@
 
 struct AppConfig : public Config, public std::enable_shared_from_this<AppConfig>
 {
-	int max_session_user_count = 3000;
 	int max_actor_monster = 10000;
 	int max_actor_npc = 1000;
 	int max_actor_object = 1000;
--- TCPServer/WorldServer/app_config.h
+++ TCPServer/WorldServer/app_config.h
@@ -3,7 +3,7 @@
 #include "../Common/config.h"
 #include "../Core/singleton.hpp"
 
-struct AppConfig : public Config, public core::pattern::Singleton<AppConfig>
+struct AppConfig : public Config, public std::enable_shared_from_this<AppConfig>
 {
 	std::size_t session_user_buffer_size = 8192;
 };
