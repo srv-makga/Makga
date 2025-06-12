6ed9568599791e420f99a4bfff1f2caf3400e573
Makga <79256138+srv-makga@users.noreply.github.com>
2025-06-12 21:14:23 +0900

    Update iocp_server.h

--- TCPServer/Common/iocp_server.h
+++ TCPServer/Common/iocp_server.h
@@ -10,11 +10,10 @@ class IocpServer : public core::network::IocpService, public std::enable_shared_
 {
 public:
 	using Service_t = core::network::IocpService;
-	using Session_t = std::shared_ptr<core::network::IocpSession>;
-	using Thread_t = std::shared_ptr<std::thread>;
+	using Session_t = core::network::IocpSession;
 
 public:
-	IocpServer();
+	IocpServer(std::shared_ptr<core::network::IocpCore> _core, std::shared_ptr<core::network::IPEndPoint> _ep);
 	IocpServer(const IocpServer& _other) = delete;
 	IocpServer(IocpServer&& _other) = delete;
 	IocpServer& operator=(const IocpServer& _other) = delete;
@@ -27,28 +26,23 @@ public: // IocpService
 	bool StartUp() override;
 	bool StartUpEnd() override;
 	bool Stop() override;
-
-	std::size_t GetMaxSessionCount() const override;
-	void SetMaxSessionCount(std::size_t _max_session_count);
-
-	const core::network::IPEndPoint& GetEndPoint() const override;
-	void SetEndPoint(const core::network::IPEndPoint& _ep) override;
+	const std::shared_ptr<core::network::IPEndPoint> GetEndPoint() const override;
+	std::size_t GetConnectCount() const override;
+	std::size_t GetMaxConnectCount() const override;
+	std::shared_ptr<Session_t> AllocSession() override;
+	void DeallocSession(std::shared_ptr<Session_t> _session) override;
 
 public:
-	bool Setup(std::shared_ptr<core::network::IocpCore> _core,
-		const core::network::IPEndPoint& _ep,
-		std::function<std::shared_ptr<core::network::IocpSession>(void)> _alloc_func,
-		std::function<void(std::shared_ptr<core::network::IocpSession>)> _dealloc_func
-	);
+	void DisconnectAllSession();
 
-private:
-	bool RunServer(std::function<void(void)> _work);
+protected:
+	bool Run(std::function<void(void)> _work) override;
 
-private:
+public:
 	std::shared_ptr<core::network::IocpAcceptor> m_acceptor;
-	core::network::IPEndPoint m_end_point;
-	std::size_t m_max_session_count = 0;
-	bool m_is_setup;
+	std::shared_ptr<core::network::IPEndPoint> m_end_point;
 
-	ThreadManager m_thread_manager;
+	mutable core::RWMutex m_mutex_session;
+	std::unordered_map<Session_t::Id, std::shared_ptr<Session_t> > m_sessions;
+	std::queue<std::shared_ptr<Session_t> > m_free_sessions;
 };
