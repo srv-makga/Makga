a851b025fec11b4b6c1661cb6d9f9772d63c6623
Makga <79256138+srv-makga@users.noreply.github.com>
2025-06-16 20:54:37 +0900

    Update net_handler.h

--- TCPServer/Common/net_handler.h
+++ TCPServer/Common/net_handler.h
@@ -1,8 +1,11 @@
 #pragma once
 
+#include "common_header.h"
+
 /*
 * @brief 네트워크 데이터가 처리될 클래스의 인터페이스 클래스
 * @detail SelectEvent, IocpEvent등의 세부 클래스가 상속함
+* @todo 추후에 IOData를 Job으로 받는 방식으로 변경할 예정?
 */
 class NetHandler
 {
@@ -12,8 +15,8 @@ public:
 
 public:
 	virtual bool Initialize() = 0;
-	virtual bool Finallize() = 0;
-	virtual bool RegistedHandle(HANDLE, ULONG_PTR) = 0;
-	virtual bool Start(std::size_t) = 0;
-	virtual bool Stop() = 0;
+	virtual void Finalize() = 0;
+	virtual bool CreateThread(std::function<void(void)> _work, std::size_t _thread_count = 1) = 0;
+	virtual bool Start() = 0;
+	virtual void Stop() = 0;
 };
