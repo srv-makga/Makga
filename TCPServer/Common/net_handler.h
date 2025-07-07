295f52938f3054d400c0a02ad2c652a47f760f07
Makga <79256138+srv-makga@users.noreply.github.com>
2025-07-08 00:32:17 +0900

    Update net_handler.h

--- TCPServer/Common/net_handler.h
+++ TCPServer/Common/net_handler.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "common_header.h"
+#include "job_handler.h"
+#include "../Core/net_core.h"
 
 /*
 * @brief 네트워크 데이터가 처리될 클래스의 인터페이스 클래스
@@ -14,9 +16,9 @@ public:
 	virtual ~NetHandler() = default;
 
 public:
-	virtual bool Initialize() = 0;
+	virtual bool Initialize(std::shared_ptr<core::network::NetCore> _net_core) = 0;
 	virtual void Finalize() = 0;
-	virtual bool CreateThread(std::function<void(void)> _work, std::size_t _thread_count = 1) = 0;
+	virtual bool CreateThread(std::size_t _thread_count = 1) = 0;
 	virtual bool Start() = 0;
 	virtual void Stop() = 0;
 };
