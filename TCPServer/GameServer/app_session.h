5b01541d5b31e18e40fb19f95abe94e96981fa7b
Makga <79256138+srv-makga@users.noreply.github.com>
2025-08-05 19:54:47 +0900

    Update app_session.h

--- TCPServer/GameServer/app_session.h
+++ TCPServer/GameServer/app_session.h
@@ -2,50 +2,22 @@
 
 #include "game_header.h"
 #include "app_config.h"
-#include "pool.h"
+#include "../Common/job_handler.h"
 
-class AppSession : public Session_t
+class AppSession : public Session_t, public JobOwner
 {
 public:
-	AppSession(core::ServiceType _type, std::size_t _buffer_size, eServerType _session_type)
-		: Session_t(_type, _buffer_size)
-		, m_session_type(_session_type)
-	{
-	}
+	AppSession(core::ServiceType _type, std::size_t _buffer_size, eServerType _session_type);
+	virtual ~AppSession();
 
-	virtual ~AppSession() = default;
+	void Send(PacketId_t _pid, FB_BUILDER& _fbb);
 
-	void Send(PacketId_t _pid, FB_BUILDER& _fbb)
-	{
-		if (0 == _fbb.GetSize())
-		{
-			return;
-		}
-
-		std::shared_ptr<core::network::NetBuffer> send_data = POOL.buffer.Pop();
-
-		send_data->Write((char*)&_pid, sizeof(_pid));
-		send_data->Write(reinterpret_cast<char*>(_fbb.GetBufferPointer()), _fbb.GetSize());
-		Session_t::Send(send_data);
-	}
-
-	void SetProcPpacket(std::function<void(std::shared_ptr<Packet>)>&& _proc_packet)
-	{
-		m_proc_packet = std::move(_proc_packet);
-	}
+public: // Session_t
+	void ProcConnect() override;
+	virtual void ProcDisconnect() override;
+	virtual std::size_t ProcRecv(char* _data, std::size_t _data_size) override;
+	virtual void ProcSend(std::size_t) override;
 
 protected:
-	void ProcPacket(std::shared_ptr<NetPacket> _packet)
-	{
-		if (nullptr == _packet || nullptr == m_proc_packet)
-		{
-			return;
-		}
-
-		m_proc_packet(_packet);
-	}
-
-protected:
-	eServerType m_session_type = eServerType_None;
-	std::function<void(std::shared_ptr<Packet>)> m_proc_packet = nullptr;
+	eServerType m_session_type;
 };
