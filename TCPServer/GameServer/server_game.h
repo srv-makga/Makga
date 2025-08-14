49ba776c81988f1b23df681672e6985bc59764d5
Makga <79256138+srv-makga@users.noreply.github.com>
2025-08-14 22:24:27 +0900

    Update server_game.h

--- TCPServer/GameServer/server_game.h
+++ TCPServer/GameServer/server_game.h
@@ -2,13 +2,14 @@
 
 #include "../core/singleton.hpp"
 #include "../Common/Server.h"
-#include "../Common/iocp_server.h"
 #include "../Common/web_client.h"
+#include "app_server.h"
 #include "app_client.h"
+#include "app_session.h"
 #include "world.h"
 #include "data_manager.h"
 
-class GameServer : public Server, public core::pattern::Singleton<GameServer>
+class GameServer : public Server, public std::enable_shared_from_this<GameServer>
 {
 public:
 	GameServer(core::ServiceType _type, std::shared_ptr<AppConfig> _config);
@@ -21,8 +22,6 @@ public:
 	bool Initialize() override;
 	void Finalize() override;
 
-	std::shared_ptr<AppClient> GetDBAgentServer() const;
-
 	std::shared_ptr<AppConfig> GetConfig() const;
 
 protected: // Server
@@ -30,20 +29,37 @@ protected: // Server
 	bool StartUpEnd() override;
 
 private:
+	bool StartConnect(fb::eServerType _type);
+	bool StartConnect(ConnectorInfo&& _info);
+	bool StartAccept(AcceptorInfo&& _info);
 	void ReleaseServer();
 	void ReleaseClient();
+
+	std::shared_ptr<AppSession> CreateSession(fb::eServerType _type);
+	
+	const core::network::IPEndPoint& GetEndPoint(eServerType _type) const;
+
+	std::shared_ptr<AppClient> FindClient(eServerType _type);
+
+public:
+	inline static std::shared_ptr<GameServer> s_server;
+	static std::shared_ptr<GameServer> Instance() { return s_server; }
+
+	static AppClient* GetDBAgentServer();
 
 private:
 	core::ServiceType m_type;
 	std::shared_ptr<AppConfig> m_config;
 	std::shared_ptr<core::network::IocpCore> m_iocp_core;
 
-	std::map<eServerType, std::shared_ptr<Server_t>> m_servers;
+	std::shared_ptr<NetHandler> m_net_handler;
+	std::shared_ptr<JobHandler> m_user_job_handler;
+	std::shared_ptr<JobHandler> m_server_job_handler;
+
+	std::map<eServerType, std::shared_ptr<AppServer>> m_servers;
 	std::map<eServerType, std::shared_ptr<AppClient>> m_clients;
 	std::map<eServerType, std::shared_ptr<WebClient>> m_web_clients;
 
 	std::shared_ptr<World> m_world;
 	std::shared_ptr<DataManager> m_data;
 };
-
-#define SERVER	GameServer::Instance()
