f2b162ae76612d9ee5571de5cafec5865bab9b13
Makga <79256138+srv-makga@users.noreply.github.com>
2025-08-18 22:11:45 +0900

    Update worker_thread.h

--- TCPServer/GameServer/worker_thread.h
+++ TCPServer/GameServer/worker_thread.h
@@ -1,34 +1,22 @@
 #pragma once
 
-#include "../Common/job_handler.h"
 #include "pool.h"
-#include <thread>
-#include <functional>
+#include "../Common/job_handler.h"
+#include "../Core/thread_pool.hpp"
 
-class WorkerThread : public JobHandler
+class WorkerThread : public JobHandler, public core::thread::ThreadPool<std::shared_ptr<Job>>
 {
 public:
 	WorkerThread(ThreadId_t _id);
 	virtual ~WorkerThread();
 
-	bool Initialize();
-	void Finalize();
+public: // JobHandler
+	bool Initialize() override;
+	void Finalize() override;
 
-	void Run(std::function<void(void)> _work);
-	void Stop();
-
-	void Push(Job_t _data) override;
-	Job_t Pop() override;
-
-
-	bool IsStop() const;
-	bool IsEmpty() const;
+	bool CreateThread(std::size_t _thread_count = 1) override;
+	void Push(TaskType _job) override;
 
 protected:
 	const ThreadId_t m_id;
-	std::thread m_thread;
-	std::atomic<bool> m_run;
-	std::queue<Job_t> m_jobs;
-	mutable std::recursive_mutex m_mutex;
-	std::condition_variable_any m_cv;
 };
