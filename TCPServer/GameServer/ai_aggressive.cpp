0fc5409ae2e02a4f313cebc9cb3ee12fd7dba97d
Makga <79256138+srv-makga@users.noreply.github.com>
2025-08-31 15:17:51 +0900

    Update ai_aggressive.cpp

--- TCPServer/GameServer/ai_aggressive.cpp
+++ TCPServer/GameServer/ai_aggressive.cpp
@@ -4,60 +4,51 @@
 #include "actor_ai.h"
 
 AIAggressive::AIAggressive(std::shared_ptr<Actor> _actor)
-	: m_actor(_actor)
-	, m_root(nullptr)
+	: AI(_actor)
 {
 }
 
 AIAggressive::~AIAggressive()
 {
-	m_actor = nullptr;
 }
 
 void AIAggressive::initialize()
 {
+	auto actor_ai = Owner()->GetActorAI();
+	
 	m_root = core::ai::Builder()
 		.composite<core::ai::Selector>()
 			.composite<core::ai::Sequence>()
-				.leaf<ActionNode>(m_actor->AI(), &ActorAI::IsDead)
+				.leaf<ActionNode>(actor_ai, &ActorAI::IsDead) // 죽은 상태라면
 				.decorator<core::ai::Succeeder>()
-					.leaf<ActionNode>(m_actor->AI(), &ActorAI::TryResurrection)
+					.leaf<ActionNode>(actor_ai, &ActorAI::TryResurrection) // 부활시도
 				.end()
 			.end()
 			.composite<core::ai::Sequence>()
-				.leaf<ActionNode>(m_actor->AI(), &ActorAI::HasTarget)
+				.leaf<ActionNode>(actor_ai, &ActorAI::HasTarget) // 타겟이 있으면
 				.composite<core::ai::Selector>()
 					.composite<core::ai::Sequence>()
-						.leaf<ActionNode>(m_actor->AI(), &ActorAI::IsInsideAttackRange)
-						.leaf<ActionNode>(m_actor->AI(), &ActorAI::AttackTarget)
+						.leaf<ActionNode>(actor_ai ,&ActorAI::IsInsideAttackRange) // 공격 범위 내인지 확인하고
+						.leaf<ActionNode>(actor_ai ,&ActorAI::AttackTarget) // 타겟 공격
 					.end()
 					.composite<core::ai::Sequence>()
-						.leaf<ActionNode>(m_actor->AI(), &ActorAI::CheckFarSpawnPoistion)
-						.leaf<ActionNode>(m_actor->AI(), &ActorAI::MoveToTarget)
+						.leaf<ActionNode>(actor_ai ,&ActorAI::CheckFarSpawnPoistion) // 스폰지역에서 멀리 떨어졌는지 확인하고
+						.leaf<ActionNode>(actor_ai ,&ActorAI::MoveToTarget) // 타겟에게 이동
 					.end()
-					.leaf<ActionNode>(m_actor->AI(), &ActorAI::ReturnRoutePosition)
+					.leaf<ActionNode>(actor_ai ,&ActorAI::ReturnRoutePosition) // 다음 좌표로 이동
 				.end()
 			.end()
 			.composite<core::ai::Sequence>()
 				.composite<core::ai::Selector>()
 					.composite<core::ai::Sequence>()
-						.leaf<ActionNode>(m_actor->AI(), &ActorAI::FindTarget)
-						.leaf<ActionNode>(m_actor->AI(), &ActorAI::SetTarget)
+						.leaf<ActionNode>(actor_ai ,&ActorAI::FindTarget) // 타겟 찾기
+						.leaf<ActionNode>(actor_ai ,&ActorAI::SetTarget) // 타겟 설정
 					.end()
-					.leaf<ActionNode>(m_actor->AI(), &ActorAI::Idle)
+					.leaf<ActionNode>(actor_ai ,&ActorAI::Idle) // 방황 상태로 변경
 				.end()
 			.end()
 		.end()
 		.build();
-}
-
-void AIAggressive::terminate(Status s)
-{
-	switch (s)
-	{
-	default:
-		break;
-	}
 }
 
 AIAggressive::Status AIAggressive::update()
