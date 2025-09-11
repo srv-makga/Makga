263f00ba0c5302b11f5639da121148b965054813
Makga <79256138+srv-makga@users.noreply.github.com>
2025-09-11 21:09:21 +0900

    Update net_buffer.h

--- TCPServer/Core/net_buffer.h
+++ TCPServer/Core/net_buffer.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include "buffer_ring.hpp"
+#include "object_pool.h"
 #include <vector>
+#include <memory>
 
 namespace core {
 namespace network {
@@ -20,6 +22,24 @@ public:
 	NetBuffer& operator=(const NetBuffer& other) = delete;
 	NetBuffer& operator=(NetBuffer&& other) = delete;
 	virtual ~NetBuffer() = default;
+
+public: // static
+	inline static std::shared_ptr<NetBuffer> Pop()
+	{
+		return s_pool.Pop();
+	}
+
+	inline static void InitPool(std::size_t _initial_size, std::function<NetBuffer*()> _create_func)
+	{
+		s_pool.Initialize(_initial_size, _create_func);
+	}
+
+	inline static void ClearPool()
+	{
+		s_pool.Finalize();
+	}
+
+	inline static core::SharedObjectPool<NetBuffer> s_pool;
 
 private:
 	std::vector<char> m_data;
