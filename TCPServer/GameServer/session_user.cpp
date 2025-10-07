10f402530c4325b51bbfabf97485c19cb84270f2
Makga <79256138+srv-makga@users.noreply.github.com>
2025-10-07 22:27:59 +0900

    Update session_user.cpp

--- TCPServer/GameServer/session_user.cpp
+++ TCPServer/GameServer/session_user.cpp
@@ -1,10 +1,8 @@
 #include "pch.h"
 #include "session_user.h"
 #include "user.h"
-#include "pool.h"
 
-//#define REG_DISPATCHER(pid)	s_dispatcher.Add(fb::server::SendPid_##pid, &SessionUser::On##pid);
-#define REG_DISPATCHER(pid)	s_dispatcher.Add(fb::server::SendPid_##pid, [](SessionUser* _session, std::shared_ptr<Packet> packet) -> bool { return _session->On##pid(packet); });
+#define REG_DISPATCHER(pid)	s_dispatcher.Add(fb::server::SendPid_##pid, &SessionUser::On##pid);
 
 core::Dispatcher<SessionUser::Pid_t, SessionUser::Function_t> SessionUser::s_dispatcher;
 
@@ -15,8 +13,8 @@ bool SessionUser::InitDispatcher()
 	return true;
 }
 
-SessionUser::SessionUser(std::size_t _buffer_size)
-	: IocpSession(core::ServiceType::IOCP_CLIENT, _buffer_size)
+SessionUser::SessionUser()
+	: SessionBase(core::BufferFlag::None, CONFIG.session_user_buffer_size)
 	, m_user(nullptr)
 {
 }
@@ -25,15 +23,32 @@ SessionUser::~SessionUser()
 {
 }
 
-bool SessionUser::Initialize()
+void SessionUser::Initialize()
 {
+	SessionBase::Initialize();
 	m_user = nullptr;
+}
+
+bool SessionUser::RecvPacket(NetPacket* _packet)
+{
+	Job* job = Job::Pop();
+	job->owner = (nullptr == GetUser()) ? (JobOwner*)this : GetUser();
+	job->packet = _packet;
+	Actor()->JobHandler()->Push(job);
 
 	return true;
 }
 
-void SessionUser::Finalize()
+bool SessionUser::ProcPacket(NetPacket* _packet)
 {
+	fb::server::SendPid pid = (fb::server::SendPid)_packet->Id();
+
+	LOG_INFO << "ProcPacket. Pid:" << fb::server::EnumNameSendPid(pid);
+
+	bool ret = s_dispatcher.Exec(pid, this, _packet);
+	LOG_ERROR_IF(false == ret) << "Fail to dispatcher exec. Pid:" << fb::server::EnumNameSendPid(pid);
+
+	return ret;
 }
 
 User* SessionUser::GetUser() const
@@ -43,16 +58,6 @@ User* SessionUser::GetUser() const
 
 void SessionUser::SetUser(User* _user)
 {
-	if (m_user == _user)
-	{
-		return;
-	}
-
-	if (nullptr != m_user)
-	{
-		m_user->SetSession(nullptr);
-	}
-
 	m_user = _user;
 
 	if (nullptr != _user)
@@ -66,56 +71,7 @@ ThreadId_t SessionUser::ThreadId() const
 	return 0;
 }
 
-void SessionUser::ProcConnect()
-{
-	LOG_INFO << "SessionId:" << GetSessionId() << ", UserSession Connected.";
-}
-
-void SessionUser::ProcDisconnect()
-{
-	LOG_INFO << "SessionId:" << GetSessionId() << ", UserSession Disconnected.";
-
-	// @todo 세션을 종료했을 떄 IOCP로 close 이벤트가 통지되고
-	// 소켓이 종료된 채로 여기에 들어오는 건지, 이벤트만 발생하고 여기서 종료 처리 해야되는지 확인 필요
-}
-
-std::size_t SessionUser::ProcRecv(char* _data, std::size_t _recv_size)
-{
-	if (nullptr == _data)
-	{
-		return 0;
-	}
-
-	std::shared_ptr<Packet> packet = Packet::Pop();
-	packet->SetData(reinterpret_cast<char*>(_data), _recv_size);
-
-	ProcPacket(packet);
-	// @todo 패킷 반납처리 (패킷 삭제처리자에 의해 반납되어야함)
-	
-	return _recv_size; // 패킷 처리에 성공하면 수신된 크기를 반환한다
-}
-
-void SessionUser::ProcSend(std::size_t _sent_size)
-{
-}
-
-void SessionUser::ProcPacket(std::shared_ptr<NetPacket> _packet)
-{
-	if (true == _packet->IsEncrypt())
-	{
-		// @todo ENCRYPTER->Decrypt(_packet->Data(), _packet->DataSize(), _packet->GetEncryptKey());
-	}
-
-	fb::server::SendPid pid = static_cast<fb::server::SendPid>(_packet->GetId());
-
-	LOG_INFO << "ProcPacket. Pid:" << fb::server::EnumNameSendPid(pid);
-
-	bool ret = s_dispatcher.Exec(pid, this, _packet);
-	
-	LOG_INFO << "ProcPacket. Pid:" << fb::server::EnumNameSendPid(pid) << ", ret:" << ret ? "true" : "false";
-}
-
-bool SessionUser::OnLoginAuth(std::shared_ptr<NetPacket> _packet)
+bool SessionUser::OnLoginAuth(NetPacket* _packet)
 {
 	return false;
 }
