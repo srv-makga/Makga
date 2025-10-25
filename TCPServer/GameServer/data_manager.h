e44a2e778d08373012050348d8b28d4317b8368e
Makga <79256138+srv-makga@users.noreply.github.com>
2025-10-25 09:56:16 +0900

    Update data_manager.h

--- TCPServer/GameServer/data_manager.h
+++ TCPServer/GameServer/data_manager.h
@@ -10,8 +10,10 @@ public:
 	bool Load() override;
 
 	const ActorBasicTable* FindActorBasicTable(MonsterIdx_t _idx) const;
+	const CharacterBasicTable* FindCharacterBasicTable(MonsterIdx_t _idx) const;
 	const MonsterBasicTable* FindMonsterBasicTable(MonsterIdx_t _idx) const;
 
 public:
 	std::map<MonsterIdx_t, MonsterBasicTable*> m_monster_basic_table;
+	std::map<TableIdx_t, CharacterBasicTable*> m_character_basic_table;
 };
