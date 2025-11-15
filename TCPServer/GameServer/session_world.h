93561bd1206fe0231d868c4468dce5969e55357f
Makga <79256138+srv-makga@users.noreply.github.com>
2025-11-15 23:17:14 +0900

    Update session_world.h

--- TCPServer/GameServer/session_world.h
+++ TCPServer/GameServer/session_world.h
@@ -1,35 +1,31 @@
 #pragma once
 
+#include "connector_world.h"
+#include "../Common/session_base.h"
 #include "../Core/dispatcher.h"
-#include "../Core/iocp_session.h"
+#include "../Core/singleton.hpp"
 
-class SessionWorld : public core::network::IocpSession
+class SessionWorld : public SessionBase, public core::pattern::Singleton<SessionWorld>
 {
 public:
 	using Pid_t = fb::world::RecvPid;
-	using Function_t = std::function<bool(SessionWorld*, std::shared_ptr<Packet>)>;
+	using Function_t = bool (SessionWorld::*)(NetPacket*);
 
 public:
 	static bool InitDispatcher();
-private:
 	inline static core::Dispatcher<Pid_t, Function_t> s_dispatcher;
 
 public:
 	SessionWorld(std::size_t _buffer_size = CONFIG.buffer_size_read);
 	virtual ~SessionWorld();
 
-public: // IocpSession
-	void OnConnected() override;
-	void OnDisconnected() override;
-	std::size_t OnRecv(char* buffer, std::size_t _length) override;
-	bool ProcPacket(std::shared_ptr<Packet> _packet) override;
-
-public:
-	ThreadId_t ThreadId() const; // JobOwner
+	bool RecvPacket(NetPacket* _packet) override; // SessionBase
+	bool ProcPacket(NetPacket* _packet) override; // JobOwner
+	ThreadId_t ThreadId() const override; // JobOwner
 
 protected: // 패킷 처리 함수
-	bool OnReg(std::shared_ptr<NetPacket> _packet);
-	bool OnPing(std::shared_ptr<NetPacket> _packet);
+	bool OnRecv_Reg(NetPacket* _packet);
+	bool OnRecv_Ping(NetPacket* _packet);
 };
 
 #define SESSION_WORLD	SessionWorld::Instance()
