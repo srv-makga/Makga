2b7e119606996f1043433d8be06e5307557830a6
Makga <79256138+srv-makga@users.noreply.github.com>
2025-11-18 23:59:22 +0900

    Update ai_non_aggressive.cpp

--- TCPServer/GameServer/ai_non_aggressive.cpp
+++ TCPServer/GameServer/ai_non_aggressive.cpp
@@ -4,49 +4,45 @@
 #include "actor_ai.h"
 
 AINonAggressive::AINonAggressive(std::shared_ptr<Actor> _actor)
-	: m_actor(_actor)
+	: AI(_actor)
 {
 }
 
 AINonAggressive::~AINonAggressive()
 {
-	m_actor = nullptr;
 }
 
 void AINonAggressive::initialize()
 {
+	auto actor_ai = Owner()->GetActorAI();
+
 	m_root = core::ai::Builder()
 		.composite<core::ai::Selector>()
 			.composite<core::ai::Sequence>()
-				.leaf<ActionNode>(m_actor->AI(), &ActorAI::IsDead)
+				.leaf<ActionNode>(actor_ai, &ActorAI::IsDead) // 죽은 상태라면
 				.decorator<core::ai::Succeeder>()
-					.leaf<ActionNode>(m_actor->AI(), &ActorAI::TryResurrection)
+					.leaf<ActionNode>(actor_ai, &ActorAI::TryResurrection) // 부활 시도
 				.end()
 			.end()
 			.composite<core::ai::Sequence>()
-				.leaf<ActionNode>(m_actor->AI(), &ActorAI::HasTarget)
+				.leaf<ActionNode>(actor_ai, &ActorAI::HasTarget) // 현재 타겟이 있으면
 				.composite<core::ai::Selector>()
 					.composite<core::ai::Sequence>()
-						.leaf<ActionNode>(m_actor->AI(), &ActorAI::IsInsideAttackRange)
-						.leaf<ActionNode>(m_actor->AI(), &ActorAI::AttackTarget)
+						.leaf<ActionNode>(actor_ai, &ActorAI::IsInsideAttackRange) // 공격 범위 내인지 판단
+						.leaf<ActionNode>(actor_ai, &ActorAI::AttackTarget) // 공격 진행
 					.end()
 					.composite<core::ai::Sequence>()
-						.leaf<ActionNode>(m_actor->AI(), &ActorAI::CheckFarSpawnPoistion)
-						.leaf<ActionNode>(m_actor->AI(), &ActorAI::MoveToTarget)
+						.leaf<ActionNode>(actor_ai, &ActorAI::CheckFarSpawnPoistion) // 혹시 스폰지역에서 멀리 떨어졌는지
+						.leaf<ActionNode>(actor_ai, &ActorAI::MoveToTarget) // 타겟에게 이동
 					.end()
-					.leaf<ActionNode>(m_actor->AI(), &ActorAI::ReturnRoutePosition)
+					.leaf<ActionNode>(actor_ai, &ActorAI::ReturnRoutePosition) // 다음 좌표로 이동
 				.end()
 			.end()
 			.composite<core::ai::Sequence>()
-				.leaf<ActionNode>(m_actor->AI(), &ActorAI::Idle)
+				.leaf<ActionNode>(actor_ai, &ActorAI::Idle) // 순회 상태로 변경
 			.end()
 		.end()
 		.build();
-}
-
-void AINonAggressive::terminate(Status s)
-{
-	m_root->terminate(Status::Running);
 }
 
 AINonAggressive::Status AINonAggressive::update()
