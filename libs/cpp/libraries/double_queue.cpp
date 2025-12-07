d2f1d9ad3a5817998727ecf379eee3a379106ac8
Makga <79256138+srv-makga@users.noreply.github.com>
2025-12-07 17:11:08 +0900

    add double queue

---
+++ libs/cpp/libraries/double_queue.cpp
@@ -0,0 +1,56 @@
+import <queue>;
+
+module makga.lib.doublequeue;
+
+namespace makga::lib {
+DoubleQueue::DoubleQueue()
+	: enqueue_(&queue1_)
+	, dequeue_(&queue2_)
+{
+}
+DoubleQueue::~DoubleQueue()
+{
+	while (false == queue1_.empty())
+	{
+		auto job = queue1_.front();
+		job();
+		queue1_.pop();
+	}
+
+	while (false == queue2_.empty())
+	{
+		auto job = queue2_.front();
+		job();
+		queue2_.pop();
+	}
+}
+
+void DoubleQueue::Push(Job_t&& job)
+{
+	std::lock_guard lock(enqueue_mutex_);
+	enqueue_->push(std::move(job));
+}
+
+DoubleQueue::Job_t DoubleQueue::Pop()
+{
+	if (true == dequeue_->empty())
+	{
+		SwapQueues();
+
+		if (true == dequeue_->empty())
+		{
+			return nullptr;
+		}
+	}
+
+	auto job = dequeue_->front();
+	dequeue_->pop();
+	return job;
+}
+
+void DoubleQueue::SwapQueues()
+{
+	std::lock_guard lock(swap_mutex_);
+	std::swap(enqueue_, dequeue_);
+}
+} // namespace makga::lib
---
+++ libs/cpp/libraries/include/double_queue.ixx
@@ -0,0 +1,34 @@
+module;
+
+export module makga.lib.doublequeue;
+
+import <queue>;
+import <functional>;
+import <mutex>;
+
+export namespace makga::lib {
+class DoubleQueue
+{
+	using Job_t = std::function<void(void)>;
+	using Queue_t = std::queue<Job_t>;
+
+public:
+	DoubleQueue();
+	virtual ~DoubleQueue();
+
+	void Push(Job_t&& job);
+	Job_t Pop();
+
+private:
+	void SwapQueues();
+
+private:
+	Queue_t* enqueue_;
+	Queue_t* dequeue_;
+	mutable std::mutex swap_mutex_;
+	mutable std::mutex enqueue_mutex_;
+
+	Queue_t queue1_;
+	Queue_t queue2_;
+};
+} // namespace makga::lib
