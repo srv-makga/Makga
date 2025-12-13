bad38833c5facf6d27161747350badb4a7beae9e
Makga <79256138+srv-makga@users.noreply.github.com>
2025-12-13 17:18:43 +0900

    Change the function to a static function

--- libs/cpp/libraries/logger.cpp
+++ libs/cpp/libraries/logger.cpp
@@ -7,12 +7,4 @@ import makga.lib.util;
 import makga.lib.pattern.singleton;
 
 namespace makga::lib {
-MakgaLogger::MakgaLogger()
-{
-	level_streams_.emplace(LogLevel::DEBUG, MakgaLoggerSteam());
-	level_streams_.emplace(LogLevel::INFO, MakgaLoggerSteam());
-	level_streams_.emplace(LogLevel::WARN, MakgaLoggerSteam());
-	level_streams_.emplace(LogLevel::ERROR, MakgaLoggerSteam());
-	level_streams_.emplace(LogLevel::FATAL, MakgaLoggerSteam());
-}
 } // namespace makga::lib
