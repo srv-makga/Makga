b727030a163c3e0ec8853ab2d7a2f412b4fb001f
Makga <79256138+srv-makga@users.noreply.github.com>
2025-12-21 00:17:38 +0900

    Update redis_connector.cpp

--- libs/cpp/libraries/src/redis_connector.cpp
+++ libs/cpp/libraries/src/redis_connector.cpp
@@ -2,6 +2,7 @@
 #include <optional>
 #include <stdexcept>
 #include <winsock2.h>
+#include <chrono>
 #include "../../3rdparty/hiredis/hiredis.h"
 
 module makga.lib.database.redis.connector;
@@ -73,6 +74,7 @@ std::optional<std::string> database::RedisConnector::Get(std::string_view key)
 	return std::string(reply->str, reply->len);
 }
 
+[[nodiscard]]
 redisReply* RedisConnector::SendCommand(std::string&& command)
 {
 	if (true == command.empty())
@@ -80,5 +82,29 @@ redisReply* RedisConnector::SendCommand(std::string&& command)
 		return nullptr;
 	}
 
+	auto context = redis_context_;
+
+	if (false == IsConnected())
+	{
+		return nullptr;
+	}
+
+	std::chrono::steady_clock::time_point start_time = std::chrono::steady_clock::now();
+	redisReply* reply = reinterpret_cast<redisReply*>(::redisCommand(redis_context_.get(), command.c_str()));
+	std::chrono::steady_clock::time_point end_time = std::chrono::steady_clock::now();
+
+	auto taken_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
+	if (100 < taken_time)
+	{
+		MakgaLogger::Warn(std::format("Redis command took too long: {0} ms, command: {1}", taken_time, command));
+	}
+
+	if (nullptr == reply)
+	{
+		MakgaLogger::Error(std::format("Redis command failed: {0}", command));
+		return nullptr;
+	}
+
+	return reply;
 }
 } // makga::lib::database
