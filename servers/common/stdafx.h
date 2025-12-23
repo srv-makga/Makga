af740ed07f2e35f972ac1991e7ca0ac43c047d7d
Makga <79256138+srv-makga@users.noreply.github.com>
2025-12-24 00:26:12 +0900

    Update stdafx.h

--- servers/common/stdafx.h
+++ servers/common/stdafx.h
@@ -1,5 +1,19 @@
 #pragma once
 
+#pragma warning(disable: 4200) // zero-sized array in struct/union
+#pragma warning(disable: 4251) // DLL-interface warning
+#pragma warning(disable: 4275) // non DLL-interface class used as base for DLL-interface class
+#pragma warning(disable: 4996) // deprecated function warning
+
+#pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "winmm.lib") // timeGetTime
+#pragma comment(lib, "iphlpapi.lib") // GetAdaptersInfo
+
+#define NOMINMAX
+
+#include <WinSock2.h>
+#include <Windows.h>
+
 #include "define.h"
 
 #include <sstream>
