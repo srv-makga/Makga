6df1c9a449773d51033116cdbf0845e5dcdf08e1
Makga <79256138+srv-makga@users.noreply.github.com>
2025-12-31 00:50:58 +0900

    Create user_dispatcher.h

---
+++ servers/game/user_dispatcher.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "dispatcher.h"
+#include "user.h"
+
+import makga.network.buffer;
+
+class UserDispatcher : public DispatcherTemplate<User>
+{
+public:
+	UserDispatcher(std::shared_ptr<User> owner);
+	virtual ~UserDispatcher() = default;
+
+	bool Execute(std::shared_ptr<User> owner, std::shared_ptr<NetPacket> packet) override;
+};
