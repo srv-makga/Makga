93e21d8eb35f80c863bd9e572385d155b19824fe
Makga <79256138+srv-makga@users.noreply.github.com>
2026-01-04 20:25:55 +0900

    플랫버퍼 파일 추가

--- scripts/flatbuffer/compile.ps1
+++ scripts/flatbuffer/compile.ps1
@@ -1,3 +1,5 @@
 Get-ChildItem *.fbs | ForEach-Object { bin\flatc.exe --cpp -o out\cpp $_.FullName }
+Copy-Item -Path ".\out\cpp\*" -Destination "..\..\servers\common\flatbuffer\" -Force
+
 Get-ChildItem *.fbs | ForEach-Object { bin\flatc.exe --csharp -o out\chsarp $_.FullName }
 Get-ChildItem *.fbs | ForEach-Object { bin\flatc.exe --go -o out\go $_.FullName }
