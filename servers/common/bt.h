7a07a20c0dcb47998099fcd05bd6c4c1a88e92a1
Makga <79256138+srv-makga@users.noreply.github.com>
2026-01-10 23:10:40 +0900

    behavior tree 파일 프로젝트 변경

--- servers/common/bt.h
+++ servers/common/bt.h
@@ -1,22 +1,22 @@
 #pragma once
 
-#include "actor.h"
+#include "bt_actor.h"
 
 import makga.lib.ai;
 
 class Bt : public makga::lib::Node
 {
 public:
-	Bt(std::shared_ptr<Actor> actor);
+	Bt(std::shared_ptr<BtActor> actor);
 	virtual ~Bt();
 
 	bool Initialize();
 	void Finalize();
 
-	std::shared_ptr<Actor> GetActor() const;
+	std::shared_ptr<BtActor> GetActor() const;
 	Node::Ptr GetRootNode() const;
 
 protected:
-	std::shared_ptr<Actor> actor_;
+	std::shared_ptr<BtActor> actor_;
 	Node::Ptr root_node_;
 };
---
+++ servers/common/bt_actor.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class BtActor
+{
+public:
+	BtActor() = default;
+	virtual ~BtActor() = default;
+};
