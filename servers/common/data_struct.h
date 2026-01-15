05c4ec6ceb73a2d2ee2e4a7a5f5e784d9a90b6fe
Makga <79256138+srv-makga@users.noreply.github.com>
2026-01-15 20:57:35 +0900

    데이터 구조체 및 타입 추가

---
+++ servers/common/data_struct.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "pch.h"
+
+struct MapTable
+{
+	TerrainIdx idx = 0;
+
+};
--- servers/common/define.h
+++ servers/common/define.h
@@ -9,9 +9,27 @@
 #include "flatbuffer/enum_generated.h"
 #include "flatbuffer/result_generated.h"
 
-using ActorId = uint32_t;
+// idx = 데이터 테이블의 고유한 인덱스
+// id = 일회성 고유한 번호. 서버가 재시작되면 다시 넘버링
+// uid = 영구적 고유한 번호. 서버가 재시작 되어도 고유함이 유지
 
 using Time = std::time_t;
+using Tick = unsigned __int64;
+
+using TableIdx = int64_t;
+
+using ChannelId = int32_t;
+
+using TerrainId = uint32_t;
+using TerrainIdx = TableIdx;
+
+using Coord = float;
+
+using ActorId = uint32_t;
+using UserUid = uint64_t;
+
+using Hp = int64_t;
+using Mp = int64_t;
 
 using ItemUid = uint64_t;
 using ItemIdx = uint64_t;
