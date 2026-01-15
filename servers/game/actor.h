c2355a9c93e839c59d0cba9d6ba891a38c730fff
Makga <79256138+srv-makga@users.noreply.github.com>
2026-01-15 20:57:19 +0900

    actor의 함수 추가

--- servers/game/actor.h
+++ servers/game/actor.h
@@ -17,13 +17,28 @@ public:
 	bool SetMovePosition(float x, float y, float z);
 	bool SetMovePosition(const makga::math::Vector3& _vector);
 
+	// BT 관련 //////////////////////////////////////////////////////////////
 	// @brief 주변에서 타켓 찾기
 	std::shared_ptr<Actor> FindTarget();
+	// @brief 현재 타겟 반환
+	std::shared_ptr<Actor> GetTarget() const;
+	// @brief 리더 반환
+	std::shared_ptr<Actor> GetLeader() const;
+	// @brief 오너 반환
+	std::shared_ptr<Actor> GetOwner() const;
+
 	// @brief 타겟 변경
 	bool ChangeTarget(std::shared_ptr<Actor> target);
+	bool HasTarget() const;
+	bool HasLeader() const;
+	bool HasOwner() const;
 
 	bool IsDead() const;
-	bool HasTarget() const;
+	bool IsAlive() const;
+	bool IsMoveable() const;
+	bool IsInSafetyArea() const;
+	bool IsInAttackRange(std::shared_ptr<Actor> target) const;
+	/// /////////////////////////////////////////////////////////////////////
 
 	makga::AIType GetAIType() const;
 
@@ -37,10 +52,14 @@ public: // get/set
 protected:
 	ActorId id_;
 	std::shared_ptr<Actor> target_;
+	std::shared_ptr<Actor> leader_;
+	std::shared_ptr<Actor> owner_;
 
 	Hp current_hp_;
 	Hp max_hp_;
 
 	Mp current_mp_;
 	Mp max_mp_;
+
+	Tick last_move_tick;
 };
