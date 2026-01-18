5f9f1318eae58181f9b47c827caa89b4410bce90
Makga <79256138+srv-makga@users.noreply.github.com>
2026-01-18 22:24:25 +0900

    포트타입 추가

--- servers/common/define.h
+++ servers/common/define.h
@@ -13,8 +13,11 @@
 // id = 일회성 고유한 번호. 서버가 재시작되면 다시 넘버링
 // uid = 영구적 고유한 번호. 서버가 재시작 되어도 고유함이 유지
 
+using Port = uint16_t;
+
 using Time = std::time_t;
 using Tick = unsigned __int64;
+
 
 using TableIdx = int64_t;
 
