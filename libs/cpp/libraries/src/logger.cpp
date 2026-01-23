2442b9b543b6ae0b85411bc741d79cc2ca8673c9
Makga <79256138+srv-makga@users.noreply.github.com>
2026-01-23 21:37:55 +0900

    cpp 파일 제거

--- libs/cpp/libraries/src/logger.cpp
+++
@@ -1,9 +0,0 @@
-module makga.lib.logger;
-
-import <string>;
-import <unordered_map>;
-import makga.lib.util;
-import makga.lib.pattern.singleton;
-
-namespace makga::lib {
-} // namespace makga::lib
