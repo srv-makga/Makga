340c353a9dbf933814966db661926087679a825b
Makga <79256138+srv-makga@users.noreply.github.com>
2026-02-07 23:04:12 +0900

    Update stat.h

--- servers/game/stat.h
+++ servers/game/stat.h
@@ -1,10 +1,28 @@
 #pragma once
 
+class Actor;
 class Stat
 {
+	using Key = uint64_t;
+	using Record = std::pair<makga::StatType, EffectValue>;
+
 public:
-	Stat();
+	Stat(std::shared_ptr<Actor> owner);
 	virtual ~Stat();
 
+	bool Initialize();
+	void Finalize();
+
 	void CopyTo(std::shared_ptr<Stat> other) const;
+
+	EffectValue GetStat(makga::StatType stat_type) const;
+	Key AddStat(makga::StatType stat_type, EffectValue value);
+	void DelStat(Key key);
+
+protected:
+	std::shared_ptr<Actor> owner_;
+	Key next_key_;
+
+	std::unordered_map<Key, Record> records_;
+	std::unordered_map<makga::StatType, EffectValue> stats_;
 };
