fdf7447114f786fab7ab6c5613d81ea5d5a7394b
Makga <79256138+srv-makga@users.noreply.github.com>
2026-02-08 20:37:06 +0900

    MessageActor 수정과 Message 타입 변경

--- servers/common/message_actor.h
+++ servers/common/message_actor.h
@@ -1,27 +1,82 @@
 #pragma once
 
+#include <memory>
+
 import makga.lib.mpsc_queue;
 
+struct Message;
 class MessageActor
 {
 public:
-	using Message = std::function<void(void)>;
-	using Queue = makga::lib::MPSCQueue<Message>;
+	using Id = uint64_t;
 
-public:
-	MessageActor();
-	virtual ~MessageActor();
+	MessageActor(Id id)
+		: id_(id) {
+	}
+	virtual ~MessageActor() = default;
 
-	bool Initialize();
-	void Finalize();
+	virtual bool IsValid() const { return false; }
 
-	void PushMessage(Message message);
-	void ProcessMessages();
+	virtual void PushMessage(std::unique_ptr<Message> message) = 0;
+	virtual void ProcessMessages(std::unique_ptr<Message> message) = 0;
 
-	Queue& GetMessageQueue();
+	void OnUpdate(float delta_time) {}
+	Id GetId() const { return id_; }
 
 protected:
-	virtual bool PopMessage(Message& message);
+	const Id id_;
+};
 
-	Queue message_queue_;
+struct Message
+{
+	enum class Type : uint16_t
+	{
+		Unknown = 0,
+		Move,
+	};
+
+	MessageActor::Id send_id = 0; // 메시지 발신자
+	MessageActor::Id id = 0; // 메시지 수신자
+	Type type = Type::Unknown;
+
+	Message(MessageActor::Id sender, MessageActor::Id receiver, Type msg_type)
+		: send_id(sender), id(receiver), type(msg_type) {
+	}
+	virtual ~Message() = default;
+	virtual void Execute(MessageActor* actor) = 0;
+	void operator()(MessageActor* actor) { Execute(actor); }
+};
+
+template<typename T>
+struct MessageT : public Message
+{
+	T data;
+
+	MessageT(MessageActor::Id sender, MessageActor::Id receiver, Type msg_type, const T& msg_data)
+		: Message(sender, receiver, msg_type), data(msg_data)
+	{
+	}
+
+	virtual void Execute(MessageActor* actor) override
+	{
+		actor->ProcessMessages(std::make_unique<MessageT<T>>(*this));
+	}
+};
+
+struct MessageLambda : public Message
+{
+	std::function<void(MessageActor*)> func;
+
+	MessageLambda(MessageActor::Id sender, MessageActor::Id receiver, std::function<void(MessageActor*)> f)
+		: Message(sender, receiver, Type::Unknown), func(f)
+	{
+	}
+
+	virtual void Execute(MessageActor* actor) override
+	{
+		if (nullptr != func)
+		{
+			func(actor);
+		}
+	}
 };
