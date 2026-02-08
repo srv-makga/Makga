4ba9f057b8735000d02c59b20d773d5c5e71b25b
Makga <79256138+srv-makga@users.noreply.github.com>
2026-02-08 20:37:13 +0900

    Create timer.h

---
+++ servers/common/timer.h
@@ -0,0 +1,29 @@
+#pragma once
+
+class Timer
+{
+	using Tick = std::chrono::milliseconds::rep;
+
+public:
+	Timer();
+	virtual ~Timer();
+	void Start();
+	void Stop();
+
+	Timer& AddJob(std::function<void()> job, Tick interval, bool is_repeat);
+
+	void OnUpdate();
+
+private:
+	struct TimerJob
+	{
+		std::function<void()> job;
+		Tick interval;
+		Tick next_execute_tick;
+		bool is_repeat;
+	};
+
+	std::multimap<Tick, TimerJob> jobs_;
+	bool is_running_;
+	Tick last_update_tick_;
+};
