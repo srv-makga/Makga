55ac23e36bd3baacd4cf5eddb4d0bc5f91220e74
Makga <79256138+srv-makga@users.noreply.github.com>
2026-02-13 00:00:06 +0900

    Update timer.h

--- servers/common/timer.h
+++ servers/common/timer.h
@@ -1,5 +1,13 @@
 #pragma once
 
+struct TimerJob
+{
+	std::function<void()> job;
+	Tick interval;
+	Tick next_execute_tick;
+	bool is_repeat;
+};
+
 class Timer
 {
 	using Tick = std::chrono::milliseconds::rep;
@@ -15,14 +23,6 @@ public:
 	void OnUpdate();
 
 private:
-	struct TimerJob
-	{
-		std::function<void()> job;
-		Tick interval;
-		Tick next_execute_tick;
-		bool is_repeat;
-	};
-
 	std::multimap<Tick, TimerJob> jobs_;
 	bool is_running_;
 	Tick last_update_tick_;
