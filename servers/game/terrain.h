9267f46e1bd4910c62bcd20a68c97e22054af4aa
Makga <79256138+srv-makga@users.noreply.github.com>
2026-02-20 23:51:37 +0900

    Update terrain.h

--- servers/game/terrain.h
+++ servers/game/terrain.h
@@ -1,35 +1,50 @@
 #pragma once
 
+#include "../common/message_actor.h"
 #include "actor.h"
 
 import makga.math.vector3;
+import makga.lib.mpsc_queue;
 
-class Actor;
 class TerrainGrid;
-class Terrain
+class Terrain : public MessageActor, public std::enable_shared_from_this<Terrain>
 {
 	friend class TerrainGrid;
-public:
-	using ActorList = std::unordered_map<ActorId, std::weak_ptr<Actor>>;
 
-	Terrain(makga::math::Vector3 min, makga::math::Vector3 max, Coord grid_size);
+public:
+	Terrain(TerrainId terrain_id, makga::math::Vector3 min, makga::math::Vector3 max, Coord grid_size);
 	virtual ~Terrain();
 
 	bool Initialize();
 	void Finalize();
 
-	
+	// @brief 위치를 받아 해당 그리드 반환
+	// @detail 높이 무시
+	TerrainGrid* GetGridAtPosition(const makga::math::Vector3& position) const;
+	TerrainGrid* GetGridAtPosition(Coord x, Coord y, Coord z) const;
 
+	// @brief n * n 을 받아 해당 그리드 반환
+	TerrainGrid* GetGridByIndex(int x, int z) const;
 	// @brief 위치가 이 Terrain 내에 있는지 확인
-	bool IsPositionInTerrain(const makga::math::Vector3& position) const;
+	bool IsInPosition(const makga::math::Vector3& position) const;
 
+	TerrainId GetTerrainId() const;
+	TerrainIdx GetTerrainIdx() const;
+
+public: // MessageActor
+	void PushMessage(std::unique_ptr<Message> message) override;
+	void ProcessMessages(std::unique_ptr<Message> message) override;
+	void OnUpdate(float delta_time) override;
+	bool IsValid() const override { return true; }
+
+protected:
 	// @brief Actor를 Terrain에 등록 (다른 Terrain에서 들어옴)
-	bool RegisterActor(ActorId actor_id, std::shared_ptr<Actor> actor, const makga::math::Vector3& position);
+	bool RegisterActor(std::shared_ptr<Actor> actor);
 	void OnRegistedActor(std::shared_ptr<Actor> actor);
 
 	// @brief Actor를 Terrain에서 제거 (다른 Terrain으로 나감)
-	bool UnregisterActor(ActorId actor_id);
-	void OnUnregistedActor(std::weak_ptr<Actor> actor);
+	bool UnregisterActor(std::shared_ptr<Actor> actor);
+	void OnUnregistedActor(std::shared_ptr<Actor> actor);
 
 	// @brief Actor가 같은 Terrain 내에서 그리드 이동
 	bool MoveActor(ActorId actor_id, const makga::math::Vector3& old_position, const makga::math::Vector3& new_position);
@@ -39,17 +54,6 @@ public:
 	// 기존이 A Terrain, 새로운 위치가 B Terrain이라면 Register,
 	// 기존이 B Terrain, 새로운 위치가 B Terrain이라면 UnRegister,
 	// 기존이 A Terrain, 새로운 위치가 A Terrain이라면 Move 처리
-
-	// @brief 위치를 받아 해당 그리드 반환
-	// @detail 높이 무시
-	TerrainGrid* GetGridAtPosition(const makga::math::Vector3& position) const;
-	TerrainGrid* GetGridAtPosition(Coord x, Coord y, Coord z) const;
-
-	// @brief n * n 을 받아 해당 그리드 반환
-	TerrainGrid* GetGridByIndex(int x, int z);
-
-	TerrainId GetTerrainId() const;
-	TerrainIdx GetTerrainIdx() const;
 
 protected:
 	void SetupNeighbors();
@@ -67,9 +71,9 @@ protected:
 
 	std::vector<std::unique_ptr<TerrainGrid>> grids_;
 
+	makga::lib::MPSCQueue<std::unique_ptr<Message>> message_queue_;
+
 	dtNavMesh* nav_mesh_; // 지형 데이터
 	static thread_local dtNavMeshQuery* nav_query_;
 	dtQueryFilter* nav_filter_; // 이동 규칙
-
-	ActorList actor_list_;
 };
