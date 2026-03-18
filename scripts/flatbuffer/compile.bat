a732e25edf2586a4a1cd925a6d5409503b7e07a6
Makga <79256138+srv-makga@users.noreply.github.com>
2026-03-18 23:45:50 +0900

    Update compile.bat

--- scripts/flatbuffer/compile.bat
+++ scripts/flatbuffer/compile.bat
@@ -1,3 +1,8 @@
 bin\flatc.exe --cpp -o out\cpp enum.fbs
+bin\flatc.exe --cpp -o out\cpp result.fbs
+bin\flatc.exe --cpp -o out\cpp version.fbs
+bin\flatc.exe --cpp -o out\cpp party_enum.fbs
 bin\flatc.exe --cpp -o out\cpp game_packet_id.fbs
 bin\flatc.exe --cpp -I . -o out\cpp game_packet.fbs
+bin\flatc.exe --cpp -o out\cpp login_packet_id.fbs
+bin\flatc.exe --cpp -I . -o out\cpp login_packet.fbs
