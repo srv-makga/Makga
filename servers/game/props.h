c03fa541e98c08972906c13cb6a8574568d4af12
Makga <79256138+srv-makga@users.noreply.github.com>
2026-03-21 00:28:33 +0900

    Create props.h

---
+++ servers/game/props.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include <memory>
+#include <functional>
+
+class Actor;
+// @brief Actor 생성 팩토리. Akka Props 개념에 대응. ActorSystem::Spawn() 등에서 Actor 인스턴스 생성에 사용.
+class Props
+{
+public:
+    using Creator = std::function<std::unique_ptr<Actor>()>;
+	Props(Creator& c);
+	Props(Creator&& c);
+	std::unique_ptr<Actor> Create() const;
+
+private:
+    Creator creator_;
+};
