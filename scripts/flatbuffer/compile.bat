06a80cad4ca8093c679a927078c805bcef98198f
Makga <79256138+srv-makga@users.noreply.github.com>
2026-03-23 22:05:35 +0900

    플랫버퍼 옵션 수정

--- scripts/flatbuffer/compile.bat
+++ scripts/flatbuffer/compile.bat
@@ -1,8 +1,7 @@
-bin\flatc.exe --cpp -o out\cpp enum.fbs
-bin\flatc.exe --cpp -o out\cpp result.fbs
-bin\flatc.exe --cpp -o out\cpp version.fbs
-bin\flatc.exe --cpp -o out\cpp party_enum.fbs
-bin\flatc.exe --cpp -o out\cpp game_packet_id.fbs
-bin\flatc.exe --cpp -I . -o out\cpp game_packet.fbs
-bin\flatc.exe --cpp -o out\cpp login_packet_id.fbs
-bin\flatc.exe --cpp -I . -o out\cpp login_packet.fbs
+bin\flatc.exe --scoped-enums --cpp -o out\cpp enum.fbs
+bin\flatc.exe --scoped-enums --cpp -o out\cpp result.fbs
+bin\flatc.exe --scoped-enums --cpp -o out\cpp version.fbs
+bin\flatc.exe --scoped-enums --cpp -o out\cpp game_packet_id.fbs
+bin\flatc.exe --scoped-enums --cpp -I . -o out\cpp game_packet.fbs
+bin\flatc.exe --scoped-enums --cpp -o out\cpp login_packet_id.fbs
+bin\flatc.exe --scoped-enums --cpp -I . -o out\cpp login_packet.fbs
