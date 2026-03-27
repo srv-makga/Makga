b1d8e7e3aa8cf715c8cb5a251747fadb4925cdb7
Makga <79256138+srv-makga@users.noreply.github.com>
2026-03-28 02:31:31 +0900

    Update pch.h

--- servers/community/pch.h
+++ servers/community/pch.h
@@ -1,6 +1,12 @@
-#pragma once
+﻿#pragma once
 
 #include "define.h"
 #include <unordered_map>
 
 import makga.lib.lock;
+import makga.lib.logger;
+import makga.lib.action_log;
+import makga.lib.log_client;
+using Logger    = makga::lib::MakgaLogger;
+using ActorLog  = makga::lib::ActionLog;
+using ActorType = makga::lib::ActorType;
